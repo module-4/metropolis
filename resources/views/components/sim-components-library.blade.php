155fa1af0f60bf687f07a6502a9c57f86fe103fe
Albert Lourensen <49406009+Mexury@users.noreply.github.com>
2025-04-22 18:37:49 +0200

    chore: Created simulation view & created components for: grid, components(functions), effects

---
+++ resources/views/components/sim-components-library.blade.php
@@ -0,0 +1,35 @@
+<div class="
+flex
+flex-col
+
+rounded-md
+p-1.5
+bg-gray-100
+col-span-1
+gap-0.5
+
+min-lg:col-start-4
+min-lg:col-span-2
+
+overflow-y-auto
+max-h-[400px]
+
+self-start
+border
+border-gray-200
+">
+    <p class="text-lg font-bold mt-2">Category name</p>
+    <x-sim-component />
+    <x-sim-component />
+    <x-sim-component />
+    <x-sim-component />
+    <x-sim-component />
+    <p class="text-lg font-bold mt-2">Category name</p>
+    <x-sim-component />
+    <x-sim-component />
+    <x-sim-component />
+    <x-sim-component />
+    <p class="text-lg font-bold mt-2">Category name</p>
+    <x-sim-component />
+    <x-sim-component />
+</div>
---
+++ resources/views/components/sim-grid.blade.php
@@ -0,0 +1,30 @@
+<div class="
+    grid
+    grid-cols-4
+    overflow-y-auto
+    overflow-hidden
+    bg-blue-950
+    border
+    border-blue-950
+    rounded-md
+    gap-0.25
+
+    col-span-1
+    min-md:col-span-2
+    min-lg:col-span-3
+    min-lg:row-span-2
+    min-lg:max-h-[400px]
+    ">
+    <x-sim-grid-tile />
+    <x-sim-grid-tile />
+    <x-sim-grid-tile />
+    <x-sim-grid-tile />
+    <x-sim-grid-tile />
+    <x-sim-grid-tile />
+    <x-sim-grid-tile />
+    <x-sim-grid-tile />
+    <x-sim-grid-tile />
+    <x-sim-grid-tile />
+    <x-sim-grid-tile />
+    <x-sim-grid-tile />
+</div>
