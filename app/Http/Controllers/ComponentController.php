343d6741220e00818e6332dc1747c739bae3272a
Jan Verhoeven <191103654+jan-verhoeven@users.noreply.github.com>
2025-05-20 13:45:00 +0200

    feature: component management update and add components

--- app/Http/Controllers/ComponentController.php
+++ app/Http/Controllers/ComponentController.php
@@ -9,12 +9,39 @@ use Illuminate\Http\Request;
 
 class ComponentController extends Controller
 {
-    public function index() {
-        $components = Component::all();
-        $effects = Effect::all();
+    public function index()
+    {
+        $components = Component::with(['category', 'effects'])->get();
         $categories = Category::all();
+        $effects = Effect::all();
 
-        return view('component-manager', ['components'=> $components, 'effects'=> $effects, 'categories' => $categories]);
+        return view('component-manager', compact('components', 'categories', 'effects'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $component = Component::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'required',
+            'image_url' => 'required',
+            'category_id' => 'required|exists:categories,id',
+            'effect_id' => 'required|exists:effects,id',
+        ]);
+
+        $component->update([
+
+            'name' => $validated['name'],
+            //todo: IMAGE_URL moet een geupload bestand zijn, waar image_url de lokale locatie is van het bestand
+            'image_url' => $validated['image_url'],
+            'category_id' => $validated['category_id'],
+        ]);
+
+        $component->effects()->sync([
+            $validated['effect_id'] => ['value' => 0.0]
+        ]);
+
+        return redirect()->route('component-manager')->with('success', 'Component updated successfully.');
     }
 
     public function store(Request $request) {
@@ -34,6 +61,7 @@ class ComponentController extends Controller
         // Attach effect via pivot table
         $component->effects()->attach($request->effect, ['value' => 0.0]);
 
-        return redirect()->route('component-manager')->with('success', 'Component created successfully.');
+        return redirect()->route('component-manager')->with('success', 'Component updated successfully.');
     }
 }
+
