f72af6790a02ea287e32f3f09250eca274d89c9e
shaherHA <shaherahmad207@gmail.com>
2025-06-02 13:11:09 +0200

    chore:added x and y coordinates label

--- resources/views/components/sim-grid-tile.blade.php
+++ resources/views/components/sim-grid-tile.blade.php
@@ -8,5 +8,8 @@
     data-y="{{$y}}"
     class="bg-primary text-white font-bold text-sm max-lg:min-h-[100px] flex justify-center items-center sim-grid-tile
 ">
+    <div class="absolute top-2 left-2 text-shadow-2xs bg-black/25 text-xs py-0.5 px-2 rounded-full z-10">
+        {{ $x }}, {{ $y }}
+    </div>
     {{ $slot }}
 </div>
